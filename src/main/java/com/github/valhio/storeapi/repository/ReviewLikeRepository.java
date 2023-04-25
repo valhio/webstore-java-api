6ec1f1b56546a91626a99b5bae5efb4bceb08ad3
valhio <valentingerasimow@gmail.com>
2023-04-25 17:03:24 +0300

    Added existsByReview_IdAndUser_UserId()

--- src/main/java/com/github/valhio/storeapi/repository/ReviewLikeRepository.java
+++ src/main/java/com/github/valhio/storeapi/repository/ReviewLikeRepository.java
@@ -1,9 +1,13 @@
 package com.github.valhio.storeapi.repository;
 
 import com.github.valhio.storeapi.model.ReviewLike;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewLikeRepository extends CrudRepository<ReviewLike, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END FROM ReviewLike r WHERE r.review.id = ?1 AND r.user.userId = ?2")
+    boolean existsByReview_IdAndUser_UserId(Long productReviewId, String userId);
 }
