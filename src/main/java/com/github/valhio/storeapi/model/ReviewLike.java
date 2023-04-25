5fe47277d4a46bcec371553526ba8e759ee9da45
valhio <valentingerasimow@gmail.com>
2023-04-25 16:18:27 +0300

    Created ReviewLike Entity

---
+++ src/main/java/com/github/valhio/storeapi/model/ReviewLike.java
@@ -0,0 +1,47 @@
+package com.github.valhio.storeapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author Valhio
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product_review")
+public class ReviewLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // FetchType.LAZY means that the entity will be loaded only when it is accessed
+    @JoinColumn(name = "review_id")
+    private ProductReview review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Date likeDate;
+
+//        private boolean liked;
+//
+//        private boolean disliked;
+//
+//        private boolean reported;
+//
+//        private boolean deleted;
+//
+//        private boolean edited;
+//
+}
