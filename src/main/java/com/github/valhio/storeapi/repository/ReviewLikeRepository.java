91a3514899eaa6721bc4bd5de982cc8005c5de26
valhio <valentingerasimow@gmail.com>
2023-04-25 18:17:22 +0300

    Added JPQL findAllByReviewId()

--- src/main/java/com/github/valhio/storeapi/repository/ReviewLikeRepository.java
+++ src/main/java/com/github/valhio/storeapi/repository/ReviewLikeRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
 
     @Query("SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END FROM ReviewLike r WHERE r.review.id = ?1 AND r.user.userId = ?2")
     boolean existsByReview_IdAndUser_UserId(Long productReviewId, String userId);
+
+    @Query("SELECT r FROM ReviewLike r WHERE r.review.id = ?1")
+    Collection<Object> findAllByReview_Id(Long productReviewId);
 }
