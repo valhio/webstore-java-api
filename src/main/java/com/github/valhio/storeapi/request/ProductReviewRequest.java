5b40df4e0c4c30b66a0a8ee30ab5a340abe0729a
valhio <valentingerasimow@gmail.com>
2023-04-25 15:07:16 +0300

    Created ProductReviewRequest model

---
+++ src/main/java/com/github/valhio/storeapi/request/ProductReviewRequest.java
@@ -0,0 +1,16 @@
+package com.github.valhio.storeapi.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductReviewRequest {
+
+    private int rating;
+    private String title;
+    private String reviewText;
+    private Long productId;
+    private String userId;
+
+}
