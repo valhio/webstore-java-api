efb1340473dcd229ef25ab960c04ce9ed3907fe7
valhio <valentingerasimow@gmail.com>
2023-04-26 17:49:47 +0300

    Added findAllByReviewId() w/ JPQL

--- src/main/java/com/github/valhio/storeapi/repository/ReviewCommentRepository.java
+++ src/main/java/com/github/valhio/storeapi/repository/ReviewCommentRepository.java
@@ -2,8 +2,14 @@ package com.github.valhio.storeapi.repository;
 
 import com.github.valhio.storeapi.model.ReviewComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Long> {
+
+    @Query("SELECT rc FROM ReviewComment rc WHERE rc.review.id = ?1")
+    List<ReviewComment> findAllByReviewId(Long productReviewId);
 }
