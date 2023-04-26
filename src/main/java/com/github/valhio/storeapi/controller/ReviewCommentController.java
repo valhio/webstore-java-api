72e81601b1616bcb93c5dbebe07d798627497386
valhio <valentingerasimow@gmail.com>
2023-04-26 18:18:03 +0300

    Added addComment() endpoint

--- src/main/java/com/github/valhio/storeapi/controller/ReviewCommentController.java
+++ src/main/java/com/github/valhio/storeapi/controller/ReviewCommentController.java
@@ -1,8 +1,14 @@
 package com.github.valhio.storeapi.controller;
 
+import com.github.valhio.storeapi.exception.domain.ProductReviewNotFoundException;
+import com.github.valhio.storeapi.exception.domain.UserNotFoundException;
+import com.github.valhio.storeapi.model.ReviewComment;
+import com.github.valhio.storeapi.model.UserPrincipal;
+import com.github.valhio.storeapi.request.ReviewCommentRequest;
 import com.github.valhio.storeapi.service.ReviewCommentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/review-comment")
@@ -14,5 +20,9 @@ public class ReviewCommentController {
         this.reviewCommentService = reviewCommentService;
     }
 
+    @PostMapping("/review/{productReviewId}/add")
+    public ResponseEntity<ReviewComment> addComment(@PathVariable Long productReviewId, @RequestBody ReviewCommentRequest reviewCommentRequest, @AuthenticationPrincipal UserPrincipal auth) throws UserNotFoundException, ProductReviewNotFoundException {
+        return ResponseEntity.ok(reviewCommentService.addComment(productReviewId, auth.getEmail(), reviewCommentRequest.getComment()));
+    }
 
 }
