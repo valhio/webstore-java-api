03650897ed27410752d4d12f3f742080e72ab6f1
valhio <valentingerasimow@gmail.com>
2023-04-26 17:52:28 +0300

    Added addComment()

--- src/main/java/com/github/valhio/storeapi/service/ReviewCommentService.java
+++ src/main/java/com/github/valhio/storeapi/service/ReviewCommentService.java
@@ -1,5 +1,7 @@
 package com.github.valhio.storeapi.service;
 
+import com.github.valhio.storeapi.exception.domain.ProductReviewNotFoundException;
+import com.github.valhio.storeapi.exception.domain.UserNotFoundException;
 import com.github.valhio.storeapi.model.ReviewComment;
 
 import java.util.List;
@@ -8,7 +10,7 @@ public interface ReviewCommentService {
 
     List<ReviewComment> findAllByReviewId(Long productReviewId);
 
-    ReviewComment addComment(Long productReviewId, String email, String comment);
+    ReviewComment addComment(Long productReviewId, String email, String comment) throws UserNotFoundException, ProductReviewNotFoundException;
 
     boolean hasCommented(Long productReviewId, String email);
 
--- src/main/java/com/github/valhio/storeapi/service/impl/ReviewCommentServiceImpl.java
+++ src/main/java/com/github/valhio/storeapi/service/impl/ReviewCommentServiceImpl.java
@@ -1,8 +1,14 @@
 package com.github.valhio.storeapi.service.impl;
 
+import com.github.valhio.storeapi.exception.domain.ProductReviewNotFoundException;
+import com.github.valhio.storeapi.exception.domain.UserNotFoundException;
+import com.github.valhio.storeapi.model.ProductReview;
 import com.github.valhio.storeapi.model.ReviewComment;
+import com.github.valhio.storeapi.model.User;
 import com.github.valhio.storeapi.repository.ReviewCommentRepository;
+import com.github.valhio.storeapi.service.ProductReviewService;
 import com.github.valhio.storeapi.service.ReviewCommentService;
+import com.github.valhio.storeapi.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,19 +17,32 @@ import java.util.List;
 public class ReviewCommentServiceImpl implements ReviewCommentService {
 
     private final ReviewCommentRepository reviewCommentRepository;
+    private final UserService userService;
+    private final ProductReviewService productReviewService;
 
-    public ReviewCommentServiceImpl(ReviewCommentRepository reviewCommentRepository) {
+    public ReviewCommentServiceImpl(ReviewCommentRepository reviewCommentRepository, UserService userService, ProductReviewService productReviewService) {
         this.reviewCommentRepository = reviewCommentRepository;
+        this.userService = userService;
+        this.productReviewService = productReviewService;
     }
 
     @Override
     public List<ReviewComment> findAllByReviewId(Long productReviewId) {
-        return null;
+        return reviewCommentRepository.findAllByReviewId(productReviewId);
     }
 
     @Override
-    public ReviewComment addComment(Long productReviewId, String email, String comment) {
-        return null;
+    public ReviewComment addComment(Long productReviewId, String email, String comment) throws UserNotFoundException, ProductReviewNotFoundException {
+        User user = userService.findUserByEmail(email);
+        ProductReview productReview = productReviewService.findById(productReviewId);
+
+        ReviewComment reviewComment = new ReviewComment();
+        reviewComment.setUser(user);
+        reviewComment.setReview(productReview);
+        reviewComment.setComment(comment);
+        reviewComment.setCommentDate(new java.util.Date());
+
+        return reviewCommentRepository.save(reviewComment);
     }
 
     @Override
